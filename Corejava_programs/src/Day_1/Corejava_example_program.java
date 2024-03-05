28e7e537c6b87a522654c2821ae94f6153b6f8e6
jagad <jagad@madfellow>
2024-03-05 15:45:37 +0530

    I HAVE FINISHED MY PROJECT

---
+++ Corejava_programs/src/Day_1/Corejava_example_program.java
@@ -0,0 +1,17 @@
+package Day_1;
+import java.util.*;
+public class Corejava_example_program {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);		
+		int a,b,c;
+		
+		System.out.println("enter the a and b values:");
+		a=s.nextInt();
+		b=s.nextInt();
+		
+		c=a+b;
+		System.out.println("addition of a and b is :"+c);
+	}
+
+}
