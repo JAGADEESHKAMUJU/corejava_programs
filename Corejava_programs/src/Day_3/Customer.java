2c0ef300653579fee3dc03c0954d73cf666543ac
jagad <jagad@madfellow>
2024-03-07 13:51:35 +0530

    I HAVE FINISHED MY PROJECT

---
+++ Corejava_programs/src/Day_3/Customer.java
@@ -0,0 +1,37 @@
+// PROGRAM TO DEMONSTRATION FOR ENTITY 
+package Day_3;
+
+public class Customer {
+	private int id;
+	private String name;
+	private String address;
+	
+	//getter and setter
+	
+
+
+	public String getName() {
+		return name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
---
+++ Corejava_programs/src/Day_3/Executor.java
@@ -0,0 +1,14 @@
+package Day_3;
+
+public class Executor {
+	public static void main(String[] args) {
+		Customer c1=new Customer();
+		c1.setId(110);
+		c1.setName("gg");
+		c1.setAddress("c");
+		
+		System.out.println("customer id is"+c1.getId()+"customer name: "+c1.getName()+"customer address: "+c1.getAddress());
+		
+	}
+
+}
